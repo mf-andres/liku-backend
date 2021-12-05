41ea99e3372cdbd5ab7bac79c40f9e2b378c5449
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2021-12-05 16:43:11 +0100

    chore: undo todo

--- liku_backend/birthday/application/gifted_gift_marker.py
+++ liku_backend/birthday/application/gifted_gift_marker.py
@@ -2,7 +2,6 @@ from liku_backend.birthday.domain.gift import Gift
 from liku_backend.birthday.domain.gift_repository import GiftRepository
 
 
-# TODO it only takes the gift id
 class GiftedGiftMarker:
     def __init__(self, gift_repository: GiftRepository):
         self.gift_repository = gift_repository
