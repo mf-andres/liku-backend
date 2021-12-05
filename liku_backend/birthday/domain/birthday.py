6588aaf8f430d254015dd412a00e69d586184d52
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2021-12-05 13:36:58 +0100

    fix: update birthday object acording to design

--- liku_backend/birthday/domain/birthday.py
+++ liku_backend/birthday/domain/birthday.py
@@ -2,8 +2,11 @@ from datetime import date
 
 
 class Birthday:
-    def __init__(self, birthday_person: str, date_: date):
+    def __init__(self, id_: str, user_id: str, birthday_person: str, date_: date):
+        self.id_ = id_
+        self.user_id = user_id
         self.birthday_person = birthday_person
         self.date_ = date_
 
 # TODO validate
+#  create vo for uuid objects
--- tests/liku_backend/utils/birthday_mother.py
+++ tests/liku_backend/utils/birthday_mother.py
@@ -4,7 +4,9 @@ from liku_backend.birthday.domain.birthday import Birthday
 
 
 def get_birthday():
+    id_ = "id_"
+    user_id = "user_id"
     birthday_person = "birthday_person"
     date_ = date.today()
-    birthday = Birthday(birthday_person, date_)
+    birthday = Birthday(id_, user_id, birthday_person, date_)
     return birthday
