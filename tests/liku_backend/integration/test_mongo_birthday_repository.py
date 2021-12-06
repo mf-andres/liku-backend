217f5eea726553a23431b1868a5fb8615ce571bd
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2021-12-06 12:00:35 +0100

    feat: mongo birthday repository updates one birthday

--- tests/liku_backend/integration/test_mongo_birthday_repository.py
+++ tests/liku_backend/integration/test_mongo_birthday_repository.py
@@ -7,3 +7,13 @@ def test_stores_one_birthday(mongo_birthday_repository_setup_and_teardown):
     mongo_birthday_repository.store(birthday)
     birthdays = mongo_birthday_repository.retrieve(birthday.user_id)
     assert len(birthdays) == 1
+
+
+def test_updates_one_birthday(mongo_birthday_repository_setup_and_teardown):
+    mongo_birthday_repository = mongo_birthday_repository_setup_and_teardown
+    birthday = birthday_mother.get_birthday()
+    mongo_birthday_repository.store(birthday)
+    birthday.birthday_person = "birthday_person_2"
+    mongo_birthday_repository.update(birthday)
+    birthdays = mongo_birthday_repository.retrieve(birthday.user_id)
+    assert birthdays[0].birthday_person == birthday.birthday_person
