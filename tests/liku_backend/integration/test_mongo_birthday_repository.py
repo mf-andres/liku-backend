1ec172facc5b38cf4e0bb3cf15b99e18fd11f22a
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2021-12-06 12:02:54 +0100

    feat: mongo birthday repository removes one birthday

--- tests/liku_backend/integration/test_mongo_birthday_repository.py
+++ tests/liku_backend/integration/test_mongo_birthday_repository.py
@@ -17,3 +17,12 @@ def test_updates_one_birthday(mongo_birthday_repository_setup_and_teardown):
     mongo_birthday_repository.update(birthday)
     birthdays = mongo_birthday_repository.retrieve(birthday.user_id)
     assert birthdays[0].birthday_person == birthday.birthday_person
+
+
+def test_removes_one_birthday(mongo_birthday_repository_setup_and_teardown):
+    mongo_birthday_repository = mongo_birthday_repository_setup_and_teardown
+    birthday = birthday_mother.get_birthday()
+    mongo_birthday_repository.store(birthday)
+    mongo_birthday_repository.remove(birthday.id_)
+    birthdays = mongo_birthday_repository.retrieve(birthday.user_id)
+    assert len(birthdays) == 0
